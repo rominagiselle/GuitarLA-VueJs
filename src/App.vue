d795faabcc9cf0ae1a07837f8a03e4d7d8ecae35
Romina <rominahelou@gmail.com>
2023-06-11 15:18:16 +0200

    :App, header, footer, methods

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { ref, reactive, onMounted } from 'vue'
+import { db } from './data/guitarra'
+import Guitarra from './components/Guitarra.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+const guitarras = ref([])
+const carrito = ref([])
+const guitarra = ref([])
+
+onMounted(() => {
+    guitarras.value = db;
+    guitarra.value = db[3]
+})
+
+const agregarCarrito = (guitarra) => {
+    const existeCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id)
+    if (existeCarrito >= 0) {
+        carrito.value[existeCarrito].cantidad++
+    } else {
+        guitarra.cantidad = 1
+        carrito.value.push(guitarra)
+    }
+}
+
+const decrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(producto => producto.id === id)
+    if (carrito.value[index].cantidad <= 1) return
+    carrito.value[index].cantidad--
+}
+
+const incrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(producto => producto.id === id)
+    if (carrito.value[index].cantidad >= 5) return
+    carrito.value[index].cantidad++
+}
+
+const eliminarProducto = (id) => {
+    carrito.value = carrito.value.filter(producto => producto.id !== id)
+}
+
+const vaciarCarrito = () => {
+   carrito.value = []
+
+}
+
+</script>
+
+<template>
+    <div>
+        <Header :carrito="carrito" :guitarra="guitarra" @incrementar-cantidad="incrementarCantidad"
+            @decrementar-cantidad="decrementarCantidad"  @agregar-carrito="agregarCarrito" @eliminar-producto="eliminarProducto" @vaciar-carrito="vaciarCarrito" />
+        <main class="container-xl mt-5">
+            <h2 class="text-center">Nuestra Colección</h2>
+            <div class="row mt-5">
+                <Guitarra v-for="guitarra in guitarras" :guitarra="guitarra" :key="guitarra"
+                    @agregar-carrito="agregarCarrito" />
+            </div>
+        </main>
+        <Footer />
+    </div>
+</template>
+
+<style scoped></style>
